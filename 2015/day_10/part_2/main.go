8627f92ba6fb489f6b67f5e4e6217a7d0bc9151d
Ali Rostami <ali.rostami@live.com>
2024-05-16 15:26:11 +0330

    added day 10 of 2015

---
+++ 2015/day_10/part_2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var input string
+	fmt.Scan(&input)
+
+	for i := 0; i < 50; i++ {
+		input = LookAndSay(input)
+	}
+
+	fmt.Printf("answer = %d", len(input))
+}
+
+func LookAndSay(input string) string {
+	result := strings.Builder{}
+	counter := 1
+	digit := input[0]
+	for i := 1; i < len(input); i++ {
+		if input[i] == digit {
+			counter++
+		} else {
+			ls := fmt.Sprintf("%d%c", counter, digit)
+			result.WriteString(ls)
+			digit = input[i]
+			counter = 1
+		}
+	}
+	result.WriteString(fmt.Sprintf("%d%c", counter, digit))
+	return result.String()
+}
